9fd62234bb5ce477f2b6e12639353722795f829d
asako <asako0128@gmail.com>
2026-02-14 18:33:07 +0900

    swiperセット

--- js/script.js
+++ js/script.js
@@ -9,5 +9,22 @@ btn.forEach((elm) => {
     });
 });
 
+// swiper
+  document.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.swiper', {
+      // 基本的な設定
+      loop: true, // ループさせる
+      pagination: {
+        el: '.swiper-pagination', // ページネーションの要素
+        clickable: true, // クリック可能にする
+      },
+      navigation: {
+        nextEl: '.swiper-button-next', // 次へボタン
+        prevEl: '.swiper-button-prev', // 前へボタン
+      },
+      
+    });
+  });
+
 
 
