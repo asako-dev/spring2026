30bc5d97ac2f9396e09d246c5abf09b1d242255a
asako <asako0128@gmail.com>
2026-02-17 19:09:36 +0900

    PC

--- js/script.js
+++ js/script.js
@@ -10,21 +10,63 @@ btn.forEach((elm) => {
 });
 
 // swiper
+  // document.addEventListener('DOMContentLoaded', () => {
+  //   const swiper = new Swiper('.swiper', {
+  //     // 基本的な設定
+  //     loop: true, // ループさせる
+  //     pagination: {
+  //       el: '.swiper-pagination', // ページネーションの要素
+  //       clickable: true, // クリック可能にする
+  //     },
+  //     navigation: {
+  //       nextEl: '.swiper-button-next', // 次へボタン
+  //       prevEl: '.swiper-button-prev', // 前へボタン
+  //     },
+
+      
+  //   });
+  // });
+
   document.addEventListener('DOMContentLoaded', () => {
-    const swiper = new Swiper('.swiper', {
-      // 基本的な設定
-      loop: true, // ループさせる
+
+  let swiper = null;
+
+  const breakpoint = window.matchMedia('(max-width: 980px)');
+
+  function enableSwiper() {
+    swiper = new Swiper('.swiper', {
+      loop: true,
       pagination: {
-        el: '.swiper-pagination', // ページネーションの要素
-        clickable: true, // クリック可能にする
+        el: '.swiper-pagination',
+        clickable: true,
       },
       navigation: {
-        nextEl: '.swiper-button-next', // 次へボタン
-        prevEl: '.swiper-button-prev', // 前へボタン
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
       },
-      
     });
-  });
+  }
+
+  function checkBreakpoint(e) {
+    if (e.matches) {
+      // スマホ → swiper有効
+      if (!swiper) {
+        enableSwiper();
+      }
+    } else {
+      // PC → swiper破棄
+      if (swiper) {
+        swiper.destroy(true, true);
+        swiper = null;
+      }
+    }
+  }
+
+  breakpoint.addEventListener('change', checkBreakpoint);
+
+  checkBreakpoint(breakpoint);
+});
+
 
 
 //アコーディオン
